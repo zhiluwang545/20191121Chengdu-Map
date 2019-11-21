58cec6c3a95a94970f120ad495579155303d7e5e
zhiluwang545 <56407099+zhiluwang545@users.noreply.github.com>
2019-11-22 03:24:02 +0800

    Add files via upload

---
+++ Mapbox-gl-map/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibGVzbGV5NTQ1NTQ1IiwiYSI6ImNrMDg3YW5ibjRlbWkzbW12NGxuNHdrZm4ifQ.-foBRQgkzeSoaM0nyCrqXA';
+var map = new mapboxgl.Map({
+     container: 'map',
+     minZoom: 10,
+     maxZoom: 17,
+     style: 'mapbox://styles/lesley545545/ck2co6ixo0o1a1cs3kzo31cts'
+
+});
